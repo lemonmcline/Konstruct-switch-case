726cf6ff1cd7011770ba9e56054f732207c70057
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-23 23:12:32 +0600

    Konstruct-switch-case
    Konstruct-switch-case

---
+++ Konstruct switch case.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int z = 0;
+    int x;
+    while (true)
+    {
+        cin >> x;
+
+        switch (z) {
+
+        case 0:
+            cout << "0";
+            z = 1;
+            break;
+
+        case 1:
+            cout << "1";
+            z = 0;
+            break;
+        }
+    }
+}
